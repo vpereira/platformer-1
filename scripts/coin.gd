701a3fe3d6af752c53ee8ebf0d918a48888b7edd
Victor Pereira <vpereirabr@gmail.com>
2026-03-29 09:01:40 +0200

    Improve Music handling

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
 
-# if we need the variable, rename it to body again
+# if we need the variable, rename it to body again 
 func _on_body_entered(_body: Node2D) -> void:
 	queue_free()
